62e164892b9a19fac1c7310d67b2b93dec633162
HulekJakub <HulekJakub@users.noreply.github.com>
2023-01-23 11:04:04 +0100

    change from query to body

--- GetLastAddedBlob/GetLastAddedBlob/GetLastAddedDistance.cs
+++ GetLastAddedBlob/GetLastAddedBlob/GetLastAddedDistance.cs
@@ -8,8 +8,8 @@ using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Collections.Generic;
 using System.Net.Http;
-
-
+using System.IO;
+using System.Text.Json;
 
 namespace GetLastAddedBlob
 {
@@ -25,13 +25,15 @@ namespace GetLastAddedBlob
             //string accountKey = "nY4AF++rbHrDT4yQ/XUsFdytvUJUBxNN+4rpeZ6RB8YbjjjhaaKvgM8dDyzuJYk7xY+6tEIXkefw+AStF9RmMA==";
 
             string MEASUREMENTS_SAS = "?sp=raud&st=2023-01-21T14:13:03Z&se=2023-02-22T14:13:00Z&sv=2021-06-08&sig=bP3yZEp6Cdd2nq5E10mtFPTznzCJRq%2Bk1mRvtxaVpXk%3D&tn=measurements";
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            RequestBody data = JsonSerializer.Deserialize<RequestBody>(requestBody);
 
             // check whether query string is valid
-            req.Query.TryGetValue("device_id", out var deviceID);
-            req.Query.TryGetValue("email", out var userEmail);
-            req.Query.TryGetValue("password", out var userPassword);
+            // req.Query.TryGetValue("device_id", out var deviceID);
+            // req.Query.TryGetValue("email", out var userEmail);
+            // req.Query.TryGetValue("password", out var userPassword);
 
-            if (userPassword.Count == 0 || userEmail.Count == 0 || deviceID.Count == 0)
+            if (data.Email == "" || data.Password == "" || data.DeviceId == "")
 
             {
                 return new BadRequestObjectResult(-1);
@@ -42,11 +44,11 @@ namespace GetLastAddedBlob
                 using HttpClient httpClient = new();
 
                 //check if user can read this measurement
-                using HttpResponseMessage idResponse = await httpClient.GetAsync($"https://shamboo-backend.azure-api.net/tableService/deviceId?email={userEmail[0]}&password={userPassword[0]}");
+                using HttpResponseMessage idResponse = await httpClient.GetAsync($"https://shamboo-backend.azure-api.net/tableService/deviceId?email={data.Email}&password={data.Password}");
                 idResponse.EnsureSuccessStatusCode();
                 string fetchedDeviceID = await idResponse.Content.ReadAsStringAsync();
 
-                if (fetchedDeviceID != deviceID[0])
+                if (fetchedDeviceID != data.DeviceId)
                 {
                     return new BadRequestObjectResult(-1);
                 }
@@ -57,7 +59,7 @@ namespace GetLastAddedBlob
                 httpClient.DefaultRequestHeaders.Add("Accept", "application/json;odata=nometadata");
                 httpClient.DefaultRequestHeaders.Add("Accept-Charset", "UTF-8");
 
-                using HttpResponseMessage distancesResponse = await httpClient.GetAsync($"https://databaseshamboo.table.core.windows.net/measurements{MEASUREMENTS_SAS}&$filter=device_id%20eq%20'{deviceID[0]}'&$select=distance,Timestamp");
+                using HttpResponseMessage distancesResponse = await httpClient.GetAsync($"https://databaseshamboo.table.core.windows.net/measurements{MEASUREMENTS_SAS}&$filter=device_id%20eq%20'{data.DeviceId}'&$select=distance,Timestamp");
                 distancesResponse.EnsureSuccessStatusCode();
                 string distances = await distancesResponse.Content.ReadAsStringAsync();
 
@@ -72,6 +74,13 @@ namespace GetLastAddedBlob
             }
 
         }
+    }
+
+    public class RequestBody
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string DeviceId { get; set; } = "";
     }
 
 
